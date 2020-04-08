7011541f830e9aaaf561f9b52870c74c5e3dd9b8
Madina <madina.karabalina@gmail.com>
2020-04-08 14:15:12 +0600

    arr

--- arrays/power of.py
+++ arrays/power of.py
@@ -1,7 +1,21 @@
-n = int(input())
- 
-while n % 3 != 0:
-    n = n//3
-        print('No')
-    
-print('yes')
+# Function to check if x is power of 2 
+def isPowerOfTwo(n): 
+	if (n == 0): 
+		return False
+	while (n != 1): 
+			if (n % 2 != 0): 
+				return False
+			n = n // 2
+
+	return True
+
+# Driver code 
+if(isPowerOfTwo(31)): 
+	print('Yes') 
+else: 
+	print('No')
+
+if(isPowerOfTwo(64)): 
+	print('Yes') 
+else: 
+	print('No') 
